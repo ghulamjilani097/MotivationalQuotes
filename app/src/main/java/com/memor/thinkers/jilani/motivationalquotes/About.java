3635c8a01d5759dd57020df565246e491b933593
Sunita <Sunitasingh775@gmail.com>
2018-03-04 02:10:03 +0530

    China Update

--- app/src/main/java/com/memor/thinkers/jilani/motivationalquotes/About.java
+++ app/src/main/java/com/memor/thinkers/jilani/motivationalquotes/About.java
@@ -29,9 +29,6 @@ public class About extends Fragment implements Helper{
         // Required empty public constructor
     }
 
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
